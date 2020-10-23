3ea5345367bde06b2197db92e20a47371fc24437
Daniel Reyes <danielckings@gmail.com>
2020-10-23 04:28:05 -0500

    Filling empty spaces on the layout based on characters by page

--- src/components/MainSection.jsx
+++ src/components/MainSection.jsx
@@ -5,6 +5,19 @@ import PaginationBox from './PaginationBox';
 
 const CHARACTERS_BY_PAGE = 12;
 
+const verifyCharactersByPage = (characterPage) => {
+    let characterPage__copy= [];
+    characterPage__copy.push(...characterPage);
+
+    if(characterPage__copy.length < CHARACTERS_BY_PAGE){
+        const body = {name:'None', image:'?', status:'None', species:'None'};
+        const fill = Array(CHARACTERS_BY_PAGE-characterPage__copy.length).fill(body);
+        characterPage__copy.push(...fill);
+    }
+
+    return characterPage__copy;
+}
+
 const MainSection = () => {
 
     const [lastId, setLastId] = useState(Infinity);
@@ -20,7 +33,7 @@ const MainSection = () => {
         (async () => {            
             resp = await fetch(`https://rickandmortyapi.com/api/character/${characterPage}`);
             resp = await resp.json();
-            setCharactersInfo(resp);
+            setCharactersInfo(verifyCharactersByPage(resp));
         })();
     },[characterPage]);
 
